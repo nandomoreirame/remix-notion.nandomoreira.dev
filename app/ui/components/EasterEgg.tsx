40dec1b32c3785df4533646956a9ea239263c2c0
Fernando Moreira <nandomoreira.me@gmail.com>
2022-04-18 05:11:49 -0300

    feat: close easter egg

--- app/ui/components/EasterEgg.tsx
+++ app/ui/components/EasterEgg.tsx
@@ -3,21 +3,26 @@ import { HiddenEasterEgg } from 'react-hidden-easter-egg';
 import { ConfettiCanvas } from 'react-raining-confetti';
 
 export function EasterEgg(): React.ReactElement {
+  const [isOpened, setOpened] = React.useState(true);
   return (
-    <HiddenEasterEgg code={['b', 'a', 't', 'm', 'a', 'n']}>
-      <ConfettiCanvas active={true} fadingMode="LIGHT" stopAfterMs={1000000} />
-      <div className="easteregg__modal">
-        <iframe
-          width="100%"
-          height="515"
-          src="https://www.youtube.com/embed/jJ5l5ls0hP4?controls=0&autoplay=1"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture"
-          allowfullscreen
-          playing={true}
-        />
-      </div>
-    </HiddenEasterEgg>
+    <>
+      {isOpened && (
+        <HiddenEasterEgg code={['b', 'a', 't', 'm', 'a', 'n']}>
+          <ConfettiCanvas active={true} fadingMode="LIGHT" stopAfterMs={1000000} />
+          <div className="easteregg__modal" onClick={() => setOpened(false)}>
+            <iframe
+              width="90%"
+              height="515"
+              src="https://www.youtube.com/embed/jJ5l5ls0hP4?controls=0&autoplay=1"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer;autoplay;clipboard-write;encrypted-media;gyroscope;picture-in-picture"
+              allowfullscreen
+              playing={true}
+            />
+          </div>
+        </HiddenEasterEgg>
+      )}
+    </>
   );
 }
